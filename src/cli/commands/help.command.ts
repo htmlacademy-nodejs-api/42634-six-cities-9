35cb194c9834455ce9b151a4976053f950d565e1
KuryakovDen <fifer2@yandex.ru>
2025-02-20 12:01:49 +0700

    Создание класса HelpCommand

---
+++ src/cli/commands/help.command.ts
@@ -0,0 +1,20 @@
+import {Command} from './commands.interface.js';
+
+export class HelpCommand implements Command {
+  public getName() {
+    return '--help';
+  }
+
+  public execute() {
+    console.info(`
+        Программа для подготовки данных для REST API сервера.
+        Пример:
+            cli.js --<command> [--arguments]
+        Команды:
+            --version:                   # выводит номер версии
+            --help:                      # печатает этот текст
+            --import <path>:             # импортирует данные из TSV
+            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+    `);
+  }
+}
