ac6e9b94d1a10f9ff7831a1cca25e6b58952c0a6
KuryakovDen <fifer2@yandex.ru>
2025-02-20 13:59:32 +0700

    Доработка CliApplication

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,9 +1,24 @@
 import {Command} from './commands/commands.interface.js';
+import {CommandParser} from './command-parser.js';
 
 type CommandCollection = Record<string, Command>;
 
 export class CliApplication {
   private commands: CommandCollection = {};
+
+  constructor(private readonly defaultCommand = '--help') {}
+
+  public getDefaultCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+    }
+
+    return this.commands[this.defaultCommand];
+  }
+
+  public getCommand(commandName: string) {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
 
   public registerCommand(commandList: Command[]) {
     commandList.forEach((command) => {
@@ -13,5 +28,14 @@ export class CliApplication {
 
       this.commands[command.getName()] = command;
     });
+  }
+
+  public processCommand(argv: string[]) {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+
+    command.execute(...commandArguments);
   }
 }
