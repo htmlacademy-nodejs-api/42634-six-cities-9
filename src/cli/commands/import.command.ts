9c0202b49f7b9241d4bbf3c7c306a65722a3faca
KuryakovDen <fifer2@yandex.ru>
2025-02-20 18:17:11 +0700

    Доработка команды импорта

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,4 +1,5 @@
 import {Command} from './commands.interface.js';
+import {TsvFileReader} from '../../shared/libs/file-reader/tsv-file-reader.js';
 
 export class ImportCommand implements Command {
   getName() {
@@ -6,6 +7,19 @@ export class ImportCommand implements Command {
   }
 
   execute(...params: string[]) {
+    const [filename] = params;
+    const fileReader = new TsvFileReader(filename.trim());
 
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        throw error;
+      }
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${error.message}`);
+    }
   }
 }
