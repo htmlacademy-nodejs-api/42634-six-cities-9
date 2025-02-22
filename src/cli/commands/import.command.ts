c7ba5625a1e0bfccfa902dc5a858ab603b179ca5
KuryakovDen <fifer2@yandex.ru>
2025-02-22 20:08:21 +0700

    Рефакторинг команды импорта

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,25 +1,37 @@
 import {Command} from './commands.interface.js';
 import {TsvFileReader} from '../../shared/libs/file-reader/tsv-file-reader.js';
+import {Offer} from '../../shared/types/offer.type.js';
+import {getErrorMessage} from '../../shared/helpers/getErrorMessage.js';
 
 export class ImportCommand implements Command {
-  getName() {
+  private onImportedOffer(offer: Offer) {
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
+  public getName() {
     return '--import';
   }
 
-  execute(...params: string[]) {
+  public async execute(...params: string[]) {
     const [filename] = params;
     const fileReader = new TsvFileReader(filename.trim());
 
+    fileReader.on('line', this.onImportedOffer);
+    fileReader.on('end', this.onCompleteImport);
+
     try {
-      fileReader.read();
-      console.log(fileReader.toArray());
+      await fileReader.read();
     } catch (error) {
       if (!(error instanceof Error)) {
         throw error;
       }
 
       console.error(`Can't import data from file: ${filename}`);
-      console.error(`Details: ${error.message}`);
+      console.error(getErrorMessage(error));
     }
   }
 }
