c75ac4386a94e96baab16e70109bde7fafa333ae
KuryakovDen <fifer2@yandex.ru>
2025-02-24 21:05:03 +0700

    Создание класса RestApplication

---
+++ src/rest/rest.application.ts
@@ -0,0 +1,10 @@
+import {Logger} from '../shared/libs/logger/logger.interface.js';
+
+export class RestApplication {
+  constructor(private readonly logger: Logger) {
+  }
+
+  public async init() {
+    this.logger.info('RestApplication initialized.');
+  }
+}
