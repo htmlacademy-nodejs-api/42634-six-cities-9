354e588339f2a74e4a46beb1501eacb72cb12373
KuryakovDen <fifer2@yandex.ru>
2025-02-25 23:40:13 +0700

    Вывод порта в консоль

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import {PinoLogger} from './shared/libs/logger/pino.logger.js';
 import {RestApplication} from './rest/rest.application.js';
+import {RestConfig} from './shared/libs/config/rest.config.js';
 
 
 async function bootstrap() {
   const logger = new PinoLogger();
-  const application = new RestApplication(logger);
+  const config = new RestConfig(logger);
+  const application = new RestApplication(logger, config);
 
   await application.init();
 }
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,9 +1,11 @@
 import {Logger} from '../shared/libs/logger/logger.interface.js';
+import {Config} from '../shared/libs/config/config.interface.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(private readonly logger: Logger, private readonly config: Config) {}
 
   public async init() {
     this.logger.info('RestApplication initialized.');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
