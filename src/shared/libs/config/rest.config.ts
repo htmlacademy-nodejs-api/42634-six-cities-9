b0b348cbfb929da0578d96ed8bc0c99725a851fb
KuryakovDen <fifer2@yandex.ru>
2025-02-25 23:35:50 +0700

    Доработка RestConfig (парсинг env-файла)

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,22 @@
 import {Config} from './config.interface.js';
+import {Logger} from '../logger/logger.interface.js';
+import {config, DotenvParseOutput} from 'dotenv';
 
 export class RestConfig implements Config {
-  constructor(private readonly config: NodeJS.ProcessEnv) {}
+  private readonly config: NodeJS.ProcessEnv;
 
-  get(key: string) {
+  constructor(private readonly logger: Logger) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get(key: string) {
     return this.config[key];
   }
 }
