aa0a93d9daeb51c6ce9671b9f132ef5e0ee0937b
KuryakovDen <fifer2@yandex.ru>
2025-02-25 23:56:11 +0700

    Рефакторинг класса RestConfig

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,8 +1,9 @@
 import {Logger} from '../shared/libs/logger/logger.interface.js';
 import {Config} from '../shared/libs/config/config.interface.js';
+import {RestSchema} from '../shared/libs/config/rest.schema.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger, private readonly config: Config) {}
+  constructor(private readonly logger: Logger, private readonly config: Config<RestSchema>) {}
 
   public async init() {
     this.logger.info('RestApplication initialized.');
