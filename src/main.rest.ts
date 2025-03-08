51ea2281ea9d740a2b4e6ed30b012bf5ce962d92
KuryakovDen <fifer2@yandex.ru>
2025-03-09 03:19:59 +0700

    Привязка DatabaseClient

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -6,6 +6,8 @@ import {Component} from './shared/types/component.enum.js';
 import {Logger} from './shared/libs/logger/logger.interface.js';
 import {Config} from './shared/libs/config/config.interface.js';
 import {RestSchema} from './shared/libs/config/rest.schema.js';
+import {DatabaseClient} from './shared/libs/database-client/database-client.interface.js';
+import {MongoDatabaseClient} from './shared/libs/database-client/mongo.database-client.js';
 
 
 async function bootstrap() {
@@ -14,6 +16,7 @@ async function bootstrap() {
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
