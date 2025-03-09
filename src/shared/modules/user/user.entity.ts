c88883de89ef31d63bae948e6ee1c3aadca93812
KuryakovDen <fifer2@yandex.ru>
2025-03-09 22:31:56 +0700

    Доработка класса UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
 import {User, UserVariant} from '../../types/user.type.js';
+import {getModelForClass, prop} from '@typegoose/typegoose';
 
 export class UserEntity implements User {
+  @prop({required: true, unique: true})
   public name: string;
+
+  @prop({required: true, unique: true})
   public email: string;
+
+  @prop({required: false, default: ''})
   public avatar?: string;
+
+  @prop({required: true})
   public password: string;
+
+  @prop({required: true})
   public userType: UserVariant;
 }
+
+export const UserModel = getModelForClass(UserEntity);
