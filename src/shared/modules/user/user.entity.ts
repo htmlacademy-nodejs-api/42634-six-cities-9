b3b24701acc030ec4c4399154631c82e7cc91ec2
KuryakovDen <fifer2@yandex.ru>
2025-03-09 22:40:36 +0700

    Доработка класса UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,6 +1,10 @@
 import {User, UserVariant} from '../../types/user.type.js';
 import {defaultClasses, getModelForClass, prop} from '@typegoose/typegoose';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({required: true, unique: true})
   public name: string;
