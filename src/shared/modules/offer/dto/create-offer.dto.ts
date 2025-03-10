0dacee3d6191f3a8384106f86c7fd559a559733b
KuryakovDen <fifer2@yandex.ru>
2025-03-10 16:06:56 +0700

    Создание CreateOfferDto

---
+++ src/shared/modules/offer/dto/create-offer.dto.ts
@@ -0,0 +1,24 @@
+import {CityType} from '../../../types/city-type.enum.js';
+import {HousingType} from '../../../types/housing-type.enum.js';
+import {ConvenienceType} from '../../../types/convenience-type.enum.js';
+import {Coordinates} from '../../../types/offer.type.js';
+
+export class CreateOfferDto {
+  public title: string;
+  public description: string;
+  public publicationDate: Date;
+  public city: CityType;
+  public previewImage: string;
+  public housingPhotos: string[];
+  public isPremium: boolean;
+  public isFavorites: boolean;
+  public rating: number;
+  public housingType: HousingType;
+  public roomsCount: number;
+  public guestsCount: number;
+  public rentalPrice: number;
+  public conveniences: ConvenienceType[];
+  public userId: string;
+  public commentsCount?: number;
+  public coordinates: Coordinates;
+}
