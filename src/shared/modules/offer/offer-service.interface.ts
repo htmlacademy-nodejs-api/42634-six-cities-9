cc977db1c9925ea157499f658387870c89d45c7e
KuryakovDen <fifer2@yandex.ru>
2025-03-14 16:41:36 +0700

    Обновление OfferService

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -4,5 +4,14 @@ import {OfferEntity} from './offer.entity.js';
 
 export interface OfferService {
   create: (dto: CreateOfferDto) => Promise<DocumentType<OfferEntity>>;
+  updateById: (offerId: string) => Promise<DocumentType<OfferEntity>>;
+  deleteById: (offerId: string) => void;
+  findOfferList: () => Promise<DocumentType<OfferEntity>[]>;
   findById: (offerId: string) => Promise<DocumentType<OfferEntity> | null>
+  findPremiumOfferList: () => Promise<DocumentType<OfferEntity>[]>;
+  findFavoritesOfferList: () => Promise<DocumentType<OfferEntity>[]>;
+  addOfferToFavorites: (offerId: string) => Promise<DocumentType<OfferEntity>>;
+  deleteOfferFromFavorites: (offerId: string) => Promise<DocumentType<OfferEntity>>;
+  incrementCommentCount: (offerId: string) => Promise<DocumentType<OfferEntity | null>>
+  exists: (offerId: string) => boolean;
 }
