1a70bb34c003390abcac17071c11718561394d52
KuryakovDen <fifer2@yandex.ru>
2025-03-18 20:36:45 +0700

    Доработка DefaultOfferService

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,17 +1,17 @@
 import {CreateOfferDto} from './dto/create-offer.dto.js';
 import {DocumentType} from '@typegoose/typegoose';
 import {OfferEntity} from './offer.entity.js';
+import {CityWithCoordinates} from '../../types/offer.type.js';
 
 export interface OfferService {
   create: (dto: CreateOfferDto) => Promise<DocumentType<OfferEntity>>;
-  updateById: (offerId: string) => Promise<DocumentType<OfferEntity>>;
+  updateById: (offerId: string, dto: CreateOfferDto) => Promise<DocumentType<OfferEntity> | null>;
   deleteById: (offerId: string) => void;
   findOfferList: () => Promise<DocumentType<OfferEntity>[]>;
   findById: (offerId: string) => Promise<DocumentType<OfferEntity> | null>
-  findPremiumOfferList: () => Promise<DocumentType<OfferEntity>[]>;
+  findPremiumOfferList: (city: CityWithCoordinates) => Promise<DocumentType<OfferEntity>[]>;
   findFavoritesOfferList: () => Promise<DocumentType<OfferEntity>[]>;
-  addOfferToFavorites: (offerId: string) => Promise<DocumentType<OfferEntity>>;
-  deleteOfferFromFavorites: (offerId: string) => Promise<DocumentType<OfferEntity>>;
-  incrementCommentCount: (offerId: string) => Promise<DocumentType<OfferEntity | null>>
-  exists: (offerId: string) => boolean;
+  addOfferToFavorites: (offerId: string) => Promise<DocumentType<OfferEntity> | null>;
+  deleteOfferFromFavorites: (offerId: string) => Promise<DocumentType<OfferEntity> | null>;
+  exists: (offerId: string) => Promise<boolean>;
 }
